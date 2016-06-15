0d0f25476401f88f128041a8d45fc4da8b8965c0
cor <cor@pruijs.nl>
2016-06-15 11:26:36 +0200

    Add random base generation

--- TreeGenerator/TreeGenerator/TreeGenerator.swift
+++ TreeGenerator/TreeGenerator/TreeGenerator.swift
@@ -16,19 +16,24 @@ struct TreeGenerator {
     let lowerDY: CGFloat = 40
     let upperDY: CGFloat = 120
     
-    let iterations = [2, 3]
-    let twigsPerIteration = [1, 2, 3]
+    let iterations = [3, 4]
+    let twigsPerIteration = [1, 2, 3, 3, 3]
     
     let flowerBaseColor = [UIColor(red: 0.8100712299346924, green: 0.1511939615011215, blue: 0.4035313427448273, alpha: 1),
-                       UIColor(red: 0.2818343937397003, green: 0.5693024396896362, blue: 0.1281824260950089, alpha: 1),
-                       UIColor(red: 0.2202886641025543, green: 0.7022308707237244, blue: 0.9593387842178345, alpha: 1),
-                       UIColor(red: 0.9346159696578979, green: 0.6284804344177246, blue: 0.107728436589241, alpha: 1)].randomItem()
+                           UIColor(red: 0.2818343937397003, green: 0.5693024396896362, blue: 0.1281824260950089, alpha: 1),
+                           UIColor(red: 0.2202886641025543, green: 0.7022308707237244, blue: 0.9593387842178345, alpha: 1),
+                           UIColor(red: 0.9346159696578979, green: 0.6284804344177246, blue: 0.107728436589241, alpha: 1)].randomItem()
     let twigBaseColor = UIColor(red: 0.501960814, green: 0.250980407, blue: 0, alpha: 1)
     
     
     func tree() -> TreeSegment {
+    
+        let base = [Twig(width: 30, direction: CGVector(dx: 0, dy: 80), color: twigBaseColor, segments: []),
+                    Twig(width: 30, direction: CGVector(dx: 5, dy: 40), color: twigBaseColor, segments: []),
+                    Twig(width: 28, direction: CGVector(dx: 10, dy: 70), color: twigBaseColor, segments: []),
+                    Twig(width: 28, direction: CGVector(dx: -10, dy: 60), color: twigBaseColor, segments: [])].randomItem()
         
-        let base = Twig(width: 30, direction: CGVector(dx: 0, dy: 80), color: twigBaseColor, segments: [])
+        
         
         iterations.randomItem().times { base.outerTwigs.forEach(self.addTwigs) }
         
